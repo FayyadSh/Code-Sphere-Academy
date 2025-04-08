3873c68e6f1a926a400f98424b174a95b369f81b
FayyadSh <fayadsh33.fs@gmail.com>
2025-04-09 00:10:16 +0300

    ..............

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -3,11 +3,14 @@ import admin from 'firebase-admin';
 
 // Check if Firebase Admin app has already been initialized to avoid duplicate initialization
 if (!admin.apps.length) {
+
+  const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
   admin.initializeApp({
     credential: admin?.credential?.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: privateKey
     }),
   });
 }
