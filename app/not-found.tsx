1a11d8c7b2a327606c7f35973a1b0a5b93a6e7cb
FayyadSh <fayadsh33.fs@gmail.com>
2025-04-09 00:53:09 +0300

    rrr

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+'use client'
 
 const NotFound = () => {
   return (
@@ -35,20 +36,6 @@ const NotFound = () => {
                   className="group inline-flex items-center font-semibold text-g1 dark:text-primary"
                 >
                   Contact Support
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="ml-4 h-6 w-6 transition-transform group-hover:translate-x-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17 8l4 4m0 0l-4 4m4-4H3"
-                    ></path>
-                  </svg>
                 </a>
               </div>
             </div>
